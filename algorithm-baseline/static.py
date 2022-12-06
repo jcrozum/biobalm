948457c0d37a0c414bf6f8ad178d0402a503a5c0
giang-trinh <giang.trinh91@example.com>
2022-12-06 14:14:31 +0100

    Fix typos in bench_pyboolnet()

--- algorithm-baseline/static.py
+++ algorithm-baseline/static.py
@@ -28,6 +28,8 @@ def get_source_nodes(network):
 def find_minimum_NFVS(network):
     nodes = []
     INx = {}
+    funs_bdd = {}
+    vars_bdd = {}
 
     for variable in network.variables():
         var_name = network.get_variable_name(variable)
@@ -38,23 +40,52 @@ def find_minimum_NFVS(network):
         fx = expr(function.replace("!", "~"))
 
         INx[var_name] = fx.support # list of nodes appearing in Boolean function fx
+
+        vx = bddvar(var_name)
+        fx = expr2bdd(expr(fx))
+
+        vars_bdd[var_name] = vx
+        funs_bdd[var_name] = fx
             
-    """Build the unsigned interaction graph"""
-    ig = nx.DiGraph()
+    """Build the unsigned and signed interaction graphs"""
+    u_ig = nx.DiGraph()
+    s_ig = nx.DiGraph()
 
     for x in nodes:
-        ig.add_node(x)
+        u_ig.add_node(x)
+        s_ig.add_node(x)
+
+        fx = funs_bdd[x]
 
         for y in INx[x]:
-            ig.add_edge(str(y), str(x)) 
+            is_actual_arc = False
 
-        """
-        TODO: node y may not actually affect node x
-        We may need to use BDDs here.
-        """
+            vy = vars_bdd[str(y)]
+            
+            fx_res_vy_0 = fx.restrict({vy: 0})
+            fx_res_vy_1 = fx.restrict({vy: 1})
+
+            if (~fx_res_vy_0 & fx_res_vy_1).satisfy_one():
+                # a positive arc with weight = 1
+                s_ig.add_edge(str(y), str(x), weight=1)
+                is_actual_arc = True
+                print(str(y) + "<1>" + str(x))
+
+            if (fx_res_vy_0 & ~fx_res_vy_1).satisfy_one():
+                # a negative arc with weight = -1
+                s_ig.add_edge(str(y), str(x), weight=-1)
+                is_actual_arc = True
+                print(str(y) + "<-1>" + str(x))
+
+            if is_actual_arc == True:
+                u_ig.add_edge(str(y), str(x))
+                #print(str(y) + "<>" + str(x))
+
 
     """Find feedback vertex set"""
-    U = FVS.FVS(ig)
+    U = FVS.FVS(u_ig)
+
+    print(U)
 
     U_neg = U # TODO: compute a minimum negative feedback vertex set
 
