6dea5559049ee22872e46eb69eb3f73f05652d31
jcrozum <jcrozum@gmail.com>
2023-04-18 10:30:04 -0400

    caches for bdd conversion, preprocessing uses dnfs

--- nfvsmotifs/state_utils.py
+++ nfvsmotifs/state_utils.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from functools import cache
 """
     Utility operations on states. State (or a subspace) is represented as a dictionary of model 
     variables mapping keys to 0/1 values.
@@ -14,12 +15,36 @@ def _state_dict_to_bdd_valuation(state: dict[str, int]) -> dict[BDDVariable, int
     """
         Convert state variables in a dictionary to their BDD counterparts.
     """
-    return { bddvar(x): y for (x,y) in state.items() }
+    return { bddvar_cache(x): y for (x,y) in state.items() }
 
-def state_to_bdd(state: dict[str, int]) -> BinaryDecisionDiagram:
+@cache
+def bddvar_cache(name: str) -> BDDVariable:
+    return bddvar(name)
+
+@cache
+def state_to_bdd_cacheable(state: frozenset[tuple[str, int]]) -> BinaryDecisionDiagram:
     """
         Convert a state variables to a BDD encoding the state singleton.
     """
+    state_bdd = BDDONE
+
+    for node, value in state:
+        node_bdd = bddvar_cache(node)
+
+        if value == 1:
+            state_bdd = state_bdd & node_bdd
+
+        if value == 0:
+            state_bdd = state_bdd & ~node_bdd
+
+    return state_bdd
+
+def state_to_bdd(state: dict[str, int], usecache=True) -> BinaryDecisionDiagram:
+    """
+        Convert a state variables to a BDD encoding the state singleton.
+    """
+    if usecache:
+        return state_to_bdd_cacheable(frozenset(state.items()))
     state_bdd = BDDONE
 
     for node, value in state.items():
@@ -77,4 +102,28 @@ def function_is_true(f: BinaryDecisionDiagram, state: dict[str, int]) -> bool:
         return False
 
     return function_restrict(f, state).is_one()
+
+def dnf_function_is_true(dnf: list[dict[str, int]], state: dict[str, int]) -> bool:
+    """
+        Returns `True` if the given DNF function evaluates to `1` for the given 
+        state (or space). 
+    """
+    if len(dnf) == 0:
+        return False
     
+    for conjunction in dnf:
+        if conjunction.items() <= state.items():
+            return True
+    return False
+
+def remove_state_from_dnf(dnf: list[dict[str, int]], state: dict[str, int]) -> list[dict[str, int]]:
+    """
+        Removes all conjunctions that are True in the state
+    """
+    modified_dnf = []
+    for conjunction in dnf:
+        if conjunction.items() <= state.items():
+            pass
+        else:
+            modified_dnf.append(conjunction)
+    return modified_dnf
