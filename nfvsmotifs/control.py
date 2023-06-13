7d874a0b61cebea6865761f400805813acb97f09
= <jrozum@binghamton.edu>
2023-06-13 11:44:21 -0400

    added new control strategy and wrapper function

--- nfvsmotifs/control.py
+++ nfvsmotifs/control.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from itertools import combinations
+from itertools import combinations, product
 from typing import cast
 
 import networkx as nx
@@ -9,12 +9,61 @@ from biodivine_aeon import BooleanNetwork
 from nfvsmotifs.space_utils import percolate_space
 from nfvsmotifs.SuccessionDiagram import SuccessionDiagram
 
+SuccessionType = list[dict[str, int]]
+ControlType = list[dict[str, int]]
+
+
+def succession_control(
+    bn: BooleanNetwork,
+    target: dict[str, int],
+    strategy: str = "internal",
+    succession_diagram: SuccessionDiagram | None = None,
+) -> list[tuple[list[ControlType], SuccessionType]]:
+    """_summary_
+
+    Parameters
+    ----------
+    bn : BooleanNetwork
+        The network to analyze, which contains the Boolean update functions.
+    target : dict[str, int]
+        The target subspace.
+    strategy : str, optional
+        The searching strategy to use to look for driver nodes. Options are
+        'internal' (default), 'all'.
+    succession_diagram : SuccessionDiagram | None, optional
+        The succession diagram from which successions will be extracted. If
+        `None`, then a succession diagram will be generated from `bn`.
+
+    Returns
+    -------
+    list[tuple[ControlType, SuccessionType]]
+        A list of control strategies. Each control strategy is represented as
+        tuple containing a list of state dictionaries and the succession from
+        which it was derived.
+    """
+    interventions: list[tuple[list[ControlType], SuccessionType]] = []
+
+    if succession_diagram is None:
+        succession_diagram = SuccessionDiagram(bn)
+
+    successions = successions_to_target(
+        succession_diagram, target=target, expand_diagram=True
+    )
+
+    for succession in successions:
+        controls = drivers_of_succession(bn, succession, strategy=strategy)
+        interventions.append((controls, succession))
+        # for cs in controls:
+        #     interventions.append((cs, succession))
+
+    return interventions
+
 
 def successions_to_target(
     succession_diagram: SuccessionDiagram,
     target: dict[str, int],
     expand_diagram: bool = True,
-) -> list[list[dict[str, int]]]:
+) -> list[SuccessionType]:
     """Find lists of nested trap spaces (successions) that lead to the
     specified target subspace.
 
@@ -30,11 +79,11 @@ def successions_to_target(
 
     Returns
     -------
-    list[list[dict[str, int]]]
+    list[SuccessionType]
         A list of successions, where each succession is a list of sequentially
         nested trap spaces that specify the target.
     """
-    successions: list[list[dict[str, int]]] = []
+    successions: list[SuccessionType] = []
 
     # expand the succession_diagram toward the target
     if expand_diagram:
@@ -75,7 +124,8 @@ def successions_to_target(
 def drivers_of_succession(
     bn: BooleanNetwork,
     succession: list[dict[str, int]],
-) -> list[list[dict[str, int]]]:
+    strategy: str = "internal",
+) -> list[ControlType]:
     """Find driver nodes of a list of sequentially nested trap spaces
 
     Parameters
@@ -84,29 +134,36 @@ def drivers_of_succession(
         The network to analyze, which contains the Boolean update functions.
     succession : list[dict[str, int]]
         A list of sequentially nested trap spaces that specify the target.
+    strategy: str
+        The searching strategy to use to look for driver nodes. Options are
+        'internal' (default), 'all'.
 
     Returns
     -------
-    list[list[dict[str, int]]]
-        A list of lists of internal driver sets, represented as state
-        dictionaries. Each list item corresponds to a list of drivers for the
-        corresponding trap space in the succession.
+    list[ControlType]
+        A list of controls. Each control is a list of lists of driver sets,
+        represented as state dictionaries. Each list item corresponds to a list
+        of drivers for the corresponding trap space in the succession.
     """
-    control_strategies: list[list[dict[str, int]]] = []
+    control_strategies: list[ControlType] = []
     assume_fixed: dict[str, int] = {}
     for ts in succession:
-        control_strategies.append(find_internal_drivers(bn, ts, assume_fixed))
-        assume_fixed.update(ts)
+        control_strategies.append(
+            find_drivers(bn, ts, strategy=strategy, assume_fixed=assume_fixed)
+        )
+        ldoi, _ = percolate_space(bn, ts | assume_fixed, strict_percolation=False)
+        assume_fixed.update(ldoi)
 
     return control_strategies
 
 
-def find_internal_drivers(
+def find_drivers(
     bn: BooleanNetwork,
     target_trap_space: dict[str, int],
+    strategy: str = "internal",
     assume_fixed: dict[str, int] | None = None,
-) -> list[dict[str, int]]:
-    """Finds internal drives of a given target trap space
+) -> ControlType:
+    """Finds drives of a given target trap space
 
     Parameters
     ----------
@@ -114,13 +171,15 @@ def find_internal_drivers(
         The network to analyze, which contains the Boolean update functions.
     target_trap_space : dict[str, int]
         The trap space we want to find drivers for.
+    strategy: str
+        The searching strategy to use to look for driver nodes. Options are
+        'internal' (default), 'all'.
     assume_fixed: dict[str,int] | None
         A dictionary of fixed variables that should be assumed to be fixed.
 
-
     Returns
     -------
-    list[dict[str, int]]
+    ControlType
         A list of internal driver sets, represented as state dictionaries.
     """
     if assume_fixed is None:
@@ -129,19 +188,36 @@ def find_internal_drivers(
     target_trap_space_inner = {
         k: v for k, v in target_trap_space.items() if k not in assume_fixed
     }
+
+    if strategy == "internal":
+        driver_pool = list(target_trap_space_inner)
+    elif strategy == "all":
+        driver_pool = [bn.get_variable_name(id) for id in bn.variables()]
+    else:
+        raise ValueError("Unknown driver search strategy")
+
     max_drivers = len(target_trap_space_inner)
-    drivers: list[dict[str, int]] = []
+    drivers: ControlType = []
     for driver_set_size in range(1, max_drivers + 1):
-        for driver_set in combinations(target_trap_space_inner, driver_set_size):
+        for driver_set in combinations(driver_pool, driver_set_size):
             if any(set(d) <= set(driver_set) for d in drivers):
                 continue
 
-            driver_dict = {k: target_trap_space_inner[k] for k in driver_set}
-            ldoi, _ = percolate_space(
-                bn, driver_dict | assume_fixed, strict_percolation=False
-            )
-
-            if target_trap_space_inner.items() <= ldoi.items():
-                drivers.append(driver_dict)
-
+            if strategy == "internal":
+                driver_dict = {k: target_trap_space_inner[k] for k in driver_set}
+                ldoi, _ = percolate_space(
+                    bn, driver_dict | assume_fixed, strict_percolation=False
+                )
+                if target_trap_space.items() <= ldoi.items():
+                    drivers.append(driver_dict)
+            elif strategy == "all":
+                for vals in product([0, 1], repeat=driver_set_size):
+                    driver_dict = {
+                        driver: value for driver, value in zip(driver_set, vals)
+                    }
+                    ldoi, _ = percolate_space(
+                        bn, driver_dict | assume_fixed, strict_percolation=False
+                    )
+                    if target_trap_space.items() <= ldoi.items():
+                        drivers.append(driver_dict)
     return drivers
--- tests/control_test.py
+++ tests/control_test.py
@@ -1,6 +1,10 @@
 from biodivine_aeon import BooleanNetwork
 
-from nfvsmotifs.control import drivers_of_succession, successions_to_target
+from nfvsmotifs.control import (
+    drivers_of_succession,
+    succession_control,
+    successions_to_target,
+)
 from nfvsmotifs.SuccessionDiagram import SuccessionDiagram
 
 
@@ -50,6 +54,9 @@ def test_basic_succession_control():
 
     # NOTE: this test is not perfect right now because
     # the ordering of the inner lists should not matter
+    # e.g.,
+    # cs = [[{"E": 1}], [{"S": 0}], [{"D": 1}, {"C: 1}], [{"B": 0}, {"A": 0}]]
+    # should also be valid
     assert drivers_of_succession(bn, target_succession) == cs
 
 
@@ -90,3 +97,83 @@ def test_basic_succession_finding():
     )
 
     assert targets_hashed == successions_hashed
+
+
+def test_internal_succession_control():
+    bn = BooleanNetwork.from_bnet(
+        """
+    S, S
+    A, S | B
+    B, A
+    C, A | D
+    D, C
+    E, false
+    """
+    )
+    target = {"S": 0, "E": 0, "A": 0, "B": 0, "C": 1, "D": 1}
+
+    true_controls = [
+        [[{"S": 0}], [{"C": 1}, {"D": 1}], [{"A": 0}, {"B": 0}]],
+        [[{"S": 0}], [{"A": 0}, {"B": 0}], [{"C": 1}, {"D": 1}]],
+    ]
+
+    true_successions = [
+        [
+            {"S": 0, "E": 0},
+            {"S": 0, "E": 0, "A": 0, "B": 0},
+            {"S": 0, "E": 0, "A": 0, "B": 0, "C": 1, "D": 1},
+        ],
+        [
+            {"S": 0, "E": 0},
+            {"S": 0, "E": 0, "C": 1, "D": 1},
+            {"S": 0, "E": 0, "A": 0, "B": 0, "C": 1, "D": 1},
+        ],
+    ]
+
+    interventions = succession_control(bn, target)
+
+    assert len(interventions) == len(true_controls)
+
+    for c, s in interventions:
+        assert c in true_controls
+        assert s in true_successions
+
+
+def test_all_succession_control():
+    bn = BooleanNetwork.from_bnet(
+        """
+    S, S
+    A, S | B
+    B, A
+    C, A | D
+    D, C
+    E, false
+    """
+    )
+    target = {"S": 0, "E": 0, "A": 0, "B": 0, "C": 1, "D": 1}
+
+    true_controls = [
+        [[{"S": 0}], [{"A": 1}, {"B": 1}, {"C": 1}, {"D": 1}], [{"A": 0}, {"B": 0}]],
+        [[{"S": 0}], [{"A": 0}, {"B": 0}], [{"C": 1}, {"D": 1}]],
+    ]
+
+    true_successions = [
+        [
+            {"S": 0, "E": 0},
+            {"S": 0, "E": 0, "A": 0, "B": 0},
+            {"S": 0, "E": 0, "A": 0, "B": 0, "C": 1, "D": 1},
+        ],
+        [
+            {"S": 0, "E": 0},
+            {"S": 0, "E": 0, "C": 1, "D": 1},
+            {"S": 0, "E": 0, "A": 0, "B": 0, "C": 1, "D": 1},
+        ],
+    ]
+
+    interventions = succession_control(bn, target, strategy="all")
+
+    assert len(interventions) == len(true_controls)
+
+    for c, s in interventions:
+        assert c in true_controls
+        assert s in true_successions
