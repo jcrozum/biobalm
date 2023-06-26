4b154096c09bca942f24f89f8c5db921e8d989dd
Samuel Pastva <sam.pastva@gmail.com>
2023-06-26 17:55:50 +0200

    Start moving benchmark code to a separate folder.

---
+++ benchmark/bench_sd_expand_bfs.py
@@ -0,0 +1,27 @@
+from biodivine_aeon import BooleanNetwork
+from nfvsmotifs.SuccessionDiagram import SuccessionDiagram
+import sys
+import nfvsmotifs
+
+# Print progress and succession diagram size.
+nfvsmotifs.SuccessionDiagram.DEBUG = True
+
+NODE_LIMIT = 1_000_000
+DEPTH_LIMIT = 10_000
+
+# This is unfortunately necessary for PyEDA Boolean expression parser (for now).
+sys.setrecursionlimit(150000)
+
+bn = BooleanNetwork.from_file(sys.argv[1])
+bn = bn.infer_regulatory_graph()
+
+# Compute the succession diagram.
+sd = SuccessionDiagram(bn)
+fully_expanded = sd.expand_bfs(bfs_level_limit=DEPTH_LIMIT, size_limit=NODE_LIMIT)
+assert fully_expanded
+
+print(f"Succession diagram size:", len(sd))
+print(f"Minimal traps:", len(sd.minimal_trap_spaces()))
+
+print("size, expanded, minimal")
+print(f"{len(sd)},{len(list(sd.expanded_ids()))},{len(sd.minimal_trap_spaces())}")
