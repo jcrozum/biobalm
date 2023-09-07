de767c807ca05fee5e5660518dffc676292043c2
Kyu Hyong Park <kjp5774@psu.edu>
2023-09-07 06:53:07 -0400

    analysis prototypes

--- analysis/SD_analysis.py
+++ analysis/SD_analysis.py
@@ -5,7 +5,7 @@ from biodivine_aeon import BooleanNetwork
 from nfvsmotifs.SuccessionDiagram import SuccessionDiagram
 import nfvsmotifs
 
-LOG_LOCATION = 'SD_analysis_'+sys.argv[1].split('/')[-2]+'.txt'
+LOG_LOCATION = 'SD_analysis_'+sys.argv[1].split('/')[-2]+'.csv'
 
 # Print progress and succession diagram size.
 nfvsmotifs.SuccessionDiagram.DEBUG = True
@@ -38,13 +38,14 @@ print(f"SD size, {len(sd)}")
 print(f"SD depth, {sd.depth()} ")
 print(f"attractors, {attractor_count}")
 print(f"motif avoidant attactors, {motif_avoidant_count}")
+print(f"minimal trapspaces, {len(sd.minimal_trap_spaces())}")
 
 log = open(LOG_LOCATION, 'a')
-log.write(sys.argv[1].split('/')[-1] + ',')  # model name
-log.write(str(bn.num_vars()) + ',')         # network size
-log.write(str(len(sd)) + ',')               # SD size
-log.write(str(sd.depth()) + ',')            # SD depth
-log.write(str(attractor_count) + ',')       # number of attractors
-log.write(str(motif_avoidant_count) + '\n') # number of motif avoidant attractors
-
+log.write(sys.argv[1].split('/')[-1] + ',')             # model name
+log.write(str(bn.num_vars()) + ',')                     # network size
+log.write(str(len(sd)) + ',')                           # SD size
+log.write(str(sd.depth()) + ',')                        # SD depth
+log.write(str(attractor_count) + ',')                   # number of attractors
+log.write(str(motif_avoidant_count) + ',')              # number of motif avoidant attractors
+log.write(str(len(sd.minimal_trap_spaces())) + '\n')    # number of minimal trapspaces
 
