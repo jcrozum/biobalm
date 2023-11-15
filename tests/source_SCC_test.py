3164718f964809ceb0a0f64f50198101fc26b844
= <jrozum@binghamton.edu>
2023-11-15 16:44:56 -0500

    fixed errors in test file

--- tests/source_SCC_test.py
+++ tests/source_SCC_test.py
@@ -1,14 +1,18 @@
-import sys
-sys.path.append("..")
-
-from nfvsmotifs._sd_algorithms.expand_source_SCCs import *
 from biodivine_aeon import BooleanNetwork
+
+from nfvsmotifs._sd_algorithms.expand_source_SCCs import (
+    expand_source_SCCs,
+    find_scc_sd,
+    find_source_nodes,
+    perc_and_remove_constants_from_bn,
+)
+from nfvsmotifs.space_utils import percolate_network, percolate_space
 from nfvsmotifs.SuccessionDiagram import SuccessionDiagram
 
-def test_find_source_nodes():
 
+def test_find_source_nodes():
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     constant1_1, (constant1_1 | !constant1_1)
     constant1_0, (constant1_0 & !constant1_0)
     constant2_1, true
@@ -16,7 +20,8 @@ def test_find_source_nodes():
     source, source
     oscillator, !oscillator
     source_after_perc, source_after_perc & constant1_1
-    after_perc_0, after_perc_0 & constant1_0""")
+    after_perc_0, after_perc_0 & constant1_0"""
+    )
 
     source_nodes = find_source_nodes(bn)
 
@@ -31,9 +36,8 @@ def test_find_source_nodes():
 
 
 def test_perc_and_remove_constants_from_bn():
-    
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     constant1_1, (constant1_1 | !constant1_1)
     constant1_0, (constant1_0 & !constant1_0)
     constant2_1, true
@@ -41,15 +45,17 @@ def test_perc_and_remove_constants_from_bn():
     source, source
     oscillator, !oscillator
     source_after_perc, source_after_perc & constant1_1
-    after_perc_0, after_perc_0 & constant1_0""")
+    after_perc_0, after_perc_0 & constant1_0"""
+    )
 
     clean_bnet, _ = perc_and_remove_constants_from_bn(bn, {})
 
     bn2 = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     source, source
     oscillator, !oscillator
-    source_after_perc, source_after_perc""")
+    source_after_perc, source_after_perc"""
+    )
 
     clean_bnet2, _ = perc_and_remove_constants_from_bn(bn2, {})
 
@@ -61,27 +67,31 @@ def test_find_scc_sd():
 A, B
 B, A | A &  C"""
 
-    scc_sd, _ = find_scc_sd(bnet, ["A", "B"], expander=SuccessionDiagram.expand_bfs, check_maa=True)
+    scc_sd, _ = find_scc_sd(
+        bnet, ["A", "B"], expander=SuccessionDiagram.expand_bfs, check_maa=True
+    )
 
     assert scc_sd.G.nodes[0]["space"] == {}
-    assert scc_sd.G.nodes[1]["space"] == {"A":0, "B":0}
-    assert scc_sd.G.nodes[2]["space"] == {"A":1, "B":1}
+    assert scc_sd.G.nodes[1]["space"] == {"A": 0, "B": 0}
+    assert scc_sd.G.nodes[2]["space"] == {"A": 1, "B": 1}
 
 
-def expansion(bn:BooleanNetwork):
+def expansion(bn: BooleanNetwork):
     sd = SuccessionDiagram(bn)
     fully_expanded = expand_source_SCCs(sd, check_maa=False)
     assert fully_expanded
 
     return bn.num_vars(), len(sd), sd.depth(), len(sd.minimal_trap_spaces())
 
+
 def test_expansion():
     # motif avoidant attractor
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     A, !A & !B | C
     B, !A & !B | C
-    C, A & B""")
+    C, A & B"""
+    )
     n, size, depth, min = expansion(bn)
     assert n == 3
     assert size == 2
@@ -90,10 +100,11 @@ def test_expansion():
 
     # motif avoidant attractor2
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     A, !C | (A & B & C)
     B, !A | (A & B & C)
-    C, !B | (A & B & C)""")
+    C, !B | (A & B & C)"""
+    )
     n, size, depth, min = expansion(bn)
     assert n == 3
     assert size == 2
@@ -102,13 +113,14 @@ def test_expansion():
 
     # motif avoidant attractors
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     A, !A & !B | C
     B, !A & !B | C
     C, A & B
     X, !Z | (X & Y & Z)
     Y, !X | (X & Y & Z)
-    Z, !Y | (X & Y & Z)""")
+    Z, !Y | (X & Y & Z)"""
+    )
     n, size, depth, min = expansion(bn)
     assert n == 6
     assert size == 3
@@ -116,11 +128,12 @@ def test_expansion():
     assert min == 1
 
     # real bnet
-    bn = BooleanNetwork.from_file("../models/bbm-bnet-inputs-true/002.bnet")
+    bn = BooleanNetwork.from_file("models/bbm-bnet-inputs-true/002.bnet")
     _, _, _, min = expansion(bn)
     assert min == 24
 
-def attractor_search(bn:BooleanNetwork):
+
+def attractor_search(bn: BooleanNetwork):
     sd = SuccessionDiagram(bn)
     fully_expanded = expand_source_SCCs(sd)
     assert fully_expanded
@@ -133,12 +146,19 @@ def attractor_search(bn:BooleanNetwork):
         if not sd.node_is_minimal(node):
             motif_avoidant_count += len(attr)
 
-    return bn.num_vars(), len(sd), sd.depth(), attractor_count, motif_avoidant_count, len(sd.minimal_trap_spaces())
+    return (
+        bn.num_vars(),
+        len(sd),
+        sd.depth(),
+        attractor_count,
+        motif_avoidant_count,
+        len(sd.minimal_trap_spaces()),
+    )
 
 
 def test_attractor_search():
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     constant1_1, (constant1_1 | !constant1_1)
     constant1_0, (constant1_0 & !constant1_0)
     constant2_1, true
@@ -146,7 +166,8 @@ def test_attractor_search():
     source, source
     oscillator, !oscillator
     source_after_perc, source_after_perc & constant1_1
-    after_perc_0, after_perc_0 & constant1_0""")
+    after_perc_0, after_perc_0 & constant1_0"""
+    )
     N, size, depth, att, maa, min = attractor_search(bn)
     assert N == 8
     assert size == 5
@@ -156,10 +177,11 @@ def test_attractor_search():
     assert min == 4
 
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     source1, source1
     source2, source2
-    oscillator, !oscillator""")
+    oscillator, !oscillator"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 3
     assert size == 5
@@ -169,8 +191,9 @@ def test_attractor_search():
     assert min == 4
 
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
-    constant, true""")
+        """targets,factors
+    constant, true"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 1
     assert size == 1
@@ -180,8 +203,9 @@ def test_attractor_search():
     assert min == 1
 
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
-    oscillator, !oscillator""")
+        """targets,factors
+    oscillator, !oscillator"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 1
     assert size == 1
@@ -191,13 +215,14 @@ def test_attractor_search():
     assert min == 1
 
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     source1, source1
     source2, source2
     A, B & source1
     B, A
     C, D & source2
-    D, C""")
+    D, C"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 6
     assert size == 15
@@ -207,13 +232,14 @@ def test_attractor_search():
     assert min == 9
 
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     source1, source1
     source2, source2
     A, B & source1
     B, A
     C, D & source2
-    D, C""")
+    D, C"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 6
     assert size == 15
@@ -224,10 +250,11 @@ def test_attractor_search():
 
     # motif avoidant attractor
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     A, !A & !B | C
     B, !A & !B | C
-    C, A & B""")
+    C, A & B"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 3
     assert size == 2
@@ -238,10 +265,11 @@ def test_attractor_search():
 
     # motif avoidant attractor2
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     A, !C | (A & B & C)
     B, !A | (A & B & C)
-    C, !B | (A & B & C)""")
+    C, !B | (A & B & C)"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 3
     assert size == 2
@@ -252,13 +280,14 @@ def test_attractor_search():
 
     # motif avoidant attractors
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     A, !A & !B | C
     B, !A & !B | C
     C, A & B
     X, !Z | (X & Y & Z)
     Y, !X | (X & Y & Z)
-    Z, !Y | (X & Y & Z)""")
+    Z, !Y | (X & Y & Z)"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 6
     assert size == 4
@@ -269,13 +298,14 @@ def test_attractor_search():
 
     # complicated combination
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     source, source
     X, Y & source
     Y, X
     A, !A & !B | C
     B, !A & !B | C
-    C, A & B & source""")
+    C, A & B & source"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 6
     assert size == 7
@@ -286,7 +316,7 @@ def test_attractor_search():
 
     # 3 levels
     bn = BooleanNetwork.from_bnet(
-    """targets,factors
+        """targets,factors
     X1, Y1
     Y1, X1
     V1, W1
@@ -299,7 +329,8 @@ def test_attractor_search():
     V2, W2 & X1
     W2, V2
     X3, Y3 & X2
-    Y3, X3""")
+    Y3, X3"""
+    )
     n, size, depth, att, maa, min = attractor_search(bn)
     assert n == 13
     assert size == 41
@@ -309,22 +340,15 @@ def test_attractor_search():
     assert min == 14
 
     # real bnet
-    bn = BooleanNetwork.from_file("../models/bbm-bnet-inputs-true/014.bnet")
+    bn = BooleanNetwork.from_file("models/bbm-bnet-inputs-true/014.bnet")
     n, size, depth, att, maa, min = attractor_search(bn)
     assert att == 2
     assert maa == 0
     assert min == 2
 
     # real bnet
-    bn = BooleanNetwork.from_file("../models/bbm-bnet-inputs-true/177.bnet")
+    bn = BooleanNetwork.from_file("models/bbm-bnet-inputs-true/177.bnet")
     n, size, depth, att, maa, min = attractor_search(bn)
     assert att == 2
     assert maa == 0
     assert min == 2
-
-
-test_find_source_nodes()
-test_perc_and_remove_constants_from_bn()
-test_find_scc_sd()
-test_expansion()
-test_attractor_search()
