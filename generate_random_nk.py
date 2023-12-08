cd3cc168e9e6da96c9b30dc5b62a7ead73899132
kyuhyongpark <kjp5774@psu.edu>
2023-12-07 19:17:53 -0500

    reorganize random models

--- generate_random_nk.py
+++ generate_random_nk.py
@@ -4,9 +4,9 @@ import pystablemotifs.random_boolean_networks as rbn
 
 N_NODES_LIST = [10, 20, 40, 80, 160, 320]
 GRAPHS_PER_SIZE = 100
-K = 2  # in-degree
-p = 0.5 # ratio of 1 in the truth table
-DIRECTORY = "models/random_nk"
+K = 3  # in-degree
+p = 0.211325 # ratio of 1 in the truth table
+DIRECTORY = "models/random_nk3"
 
 for N_NODES in N_NODES_LIST:
     rules_list = rbn.random_boolean_network_ensemble_kauffman(N_NODES,K,p,GRAPHS_PER_SIZE,seed=1000)
