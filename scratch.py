51f86dac49041f177b64aaae0f8799270ae66ee0
= <jrozum@binghamton.edu>
2023-12-12 16:40:20 -0500

    pickle support

--- scratch.py
+++ scratch.py
@@ -1,6 +1,5 @@
 # type: ignore
-
-# import pickle
+import pickle
 
 from balm.SuccessionDiagram import SuccessionDiagram
 
@@ -12,3 +11,9 @@ C, A & B"""
 sd = SuccessionDiagram.from_bnet(rules)
 sd.build()
 print(sd.summary())
+
+pickle.dump(sd, open("sd.pickle", "wb"))
+
+sd2 = pickle.load(open("sd.pickle", "rb"))
+
+print(sd2.summary())
