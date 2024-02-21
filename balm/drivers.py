fd3bba1ea050a0a44c3133609db3e8f200802a31
= <jrozum@binghamton.edu>
2024-02-21 17:27:06 -0500

    docstring for find_single_node_LDOIs

--- balm/drivers.py
+++ balm/drivers.py
@@ -7,36 +7,68 @@ from balm.types import BooleanSpace
 
 
 def find_single_node_LDOIs(
-    stg: AsynchronousGraph | BooleanNetwork,
+    network: AsynchronousGraph | BooleanNetwork,
 ) -> dict[tuple[str, int], BooleanSpace]:
     """
-    Finds the LDOIs of every single node state.
+    Finds the the logical domain of influence for every single node state.
 
-    This operation requires the symbolic update functions provided by an `AsynchronousGraph`.
-    If you provide a `BooleanNetwork`, the `AsynchronousGraph` will be created automatically,
-    but this can incur additional overhead.
+    The logical domain of influence is equivalent to the set of node states that
+    become fixed after percolation. See
+    :func:`percolate_space<balm.space_utils.percolate_space>` for details.
 
-    TODO: take an initial set of LDOIs (e.g., of the original system) as an argument for speed-up
+    This operation requires the symbolic update functions provided by an
+    `AsynchronousGraph`. If you provide a `BooleanNetwork`, the
+    `AsynchronousGraph` will be created automatically, but this can incur
+    additional overhead.
+
+    Parameters
+    ----------
+    network : AsynchronousGraph | BooleanNetwork
+        The symbolic update functions stored as a `AsynchronousGraph` or
+        `BooleanNetwork` object from the `biodivine_aeon` library.
+
+    Returns
+    -------
+    dict[tuple[str, int], BooleanSpace]
+        The LDOI for every one-node state. The keys are `(variable, value)`
+        tuples, where `variable` is the name of the node that is fixed in the
+        state `value` (`0` or `1`). The keys are
+        :class:`BooleanSpace<balm.types.BooleanSpace>` objects, which are dictionaries
+        of node values describing the node states that become fixed as a result
+        of percolation.
+
+    Example
+    _______
+    >>> ldois = balm.drivers.find_single_node_LDOIs(sd.network)
+    >>> sd = balm.SuccessionDiagram.from_bnet('A, A\\nB, A')
+    >>> ldois = balm.drivers.find_single_node_LDOIs(sd.network)
+    >>> for k in sorted(ldois):
+    ...     print(f'{k} ==> {sorted(ldois[k].items())}')
+    ...
+    ('A', 0) ==> [('A', 0), ('B', 0)]
+    ('A', 1) ==> [('A', 1), ('B', 1)]
+    ('B', 0) ==> []
+    ('B', 1) ==> []
     """
-    if isinstance(stg, BooleanNetwork):
-        stg = AsynchronousGraph(stg)
+    if isinstance(network, BooleanNetwork):
+        network = AsynchronousGraph(network)
 
     LDOIs: dict[tuple[str, int], BooleanSpace] = {}
-    for var in stg.network_variable_names():
-        fn_bdd = stg.mk_update_function(var)
+    for var in network.network_variable_names():
+        fn_bdd = network.mk_update_function(var)
         if fn_bdd.is_true() or fn_bdd.is_false():
             # Skip constant nodes.
             continue
 
-        LDOIs[(var, 0)] = percolate_space_strict(stg, {var: 0})
-        LDOIs[(var, 1)] = percolate_space_strict(stg, {var: 1})
+        LDOIs[(var, 0)] = percolate_space_strict(network, {var: 0})
+        LDOIs[(var, 1)] = percolate_space_strict(network, {var: 1})
 
     return LDOIs
 
 
 def find_single_drivers(
     target_subspace: BooleanSpace,
-    stg: AsynchronousGraph | BooleanNetwork,
+    network: AsynchronousGraph | BooleanNetwork,
     LDOIs: dict[tuple[str, int], BooleanSpace] | None = None,
 ) -> set[tuple[str, int]]:
     """
@@ -47,11 +79,11 @@ def find_single_drivers(
     If you provide a `BooleanNetwork`, the `AsynchronousGraph` will be created automatically,
     but this can incur additional overhead.
     """
-    if isinstance(stg, BooleanNetwork):
-        stg = AsynchronousGraph(stg)
+    if isinstance(network, BooleanNetwork):
+        network = AsynchronousGraph(network)
 
     if LDOIs is None:
-        LDOIs = find_single_node_LDOIs(stg)
+        LDOIs = find_single_node_LDOIs(network)
 
     drivers: set[tuple[str, int]] = set()
     for fix, LDOI in LDOIs.items():
