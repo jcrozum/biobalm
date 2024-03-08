e31cf20e984564e153cfce244ca2814b4417483a
Jordan Rozum <jrozum@binghamton.edu>
2024-03-08 12:11:42 -0500

    symbolic_utils docs

--- balm/symbolic_utils.py
+++ balm/symbolic_utils.py
@@ -1,5 +1,5 @@
 """
-    Utility operations for creating and manipulating symbolic functions.
+Utility operations for creating and manipulating symbolic functions.
 """
 
 from __future__ import annotations
@@ -16,29 +16,87 @@ from biodivine_aeon import BddVariableSet
 from balm.types import BooleanSpace
 
 
-def state_to_bdd(ctx: SymbolicContext | BddVariableSet, state: BooleanSpace) -> Bdd:
+def state_to_bdd(
+    bdd_context: SymbolicContext | BddVariableSet, state: BooleanSpace
+) -> Bdd:
     """
     Convert a `BooleanSpace` to a BDD encoding the state singleton.
+
+    Parameters
+    ----------
+    bdd_context : SymbolicContext | BddVariableSet
+        The context in which the state is represented. This can be either a
+        `biodivine_aeon.SymbolicContext` or a `biodivine_aeon.BddVariableSet`.
+        If the former is given, it is converted to the latter. The context is
+        used to ensure compatibility between BDDs by ensuring that the same
+        variable names and ordering are used.
+    state : BooleanSpace
+        The state to convert to a BDD.
+
+    Returns
+    -------
+    Bdd
+        The BDD encoding the given state.
     """
-    bdd = ctx if isinstance(ctx, BddVariableSet) else ctx.bdd_variable_set()
+    bdd = (
+        bdd_context
+        if isinstance(bdd_context, BddVariableSet)
+        else bdd_context.bdd_variable_set()
+    )
     return bdd.mk_conjunctive_clause(state)
 
 
 def state_list_to_bdd(
-    ctx: SymbolicContext | BddVariableSet, states: list[BooleanSpace]
+    bdd_context: SymbolicContext | BddVariableSet, states: list[BooleanSpace]
 ) -> Bdd:
     """
     Convert a list of state dictionaries to a BDD representation.
+
+    The BDD will be satisfied in any state in the list and in no others.
+
+    Parameters
+    ----------
+    bdd_context : SymbolicContext | BddVariableSet
+        The context in which the state is represented. This can be either a
+        `biodivine_aeon.SymbolicContext` or a `biodivine_aeon.BddVariableSet`.
+        If the former is given, it is converted to the latter. The context is
+        used to ensure compatibility between BDDs by ensuring that the same
+        variable names and ordering are used.
+    states : list[BooleanSpace]
+        The list of states to convert to a BDD.
+
+    Returns
+    -------
+    Bdd
+        The BDD encoding the given list of states.
     """
-    bdd = ctx if isinstance(ctx, BddVariableSet) else ctx.bdd_variable_set()
+    bdd = (
+        bdd_context
+        if isinstance(bdd_context, BddVariableSet)
+        else bdd_context.bdd_variable_set()
+    )
     return bdd.mk_dnf(states)
 
 
 def function_eval(f: Bdd, state: BooleanSpace) -> Literal[0, 1] | None:
     """
-    Evaluate a BDD function in the given state to an integer value. If the state
-    is incomplete (i.e. it is a space), the function may not evaluate to an
-    exact integer. In such case, `None` is returned.
+    Evaluate a BDD Boolean function in the given state.
+
+    If the state is incomplete (i.e. it is a space), the function value can be
+    undetermined. In such case, `None` is returned.
+
+    Parameters
+    ----------
+    f : Bdd
+        The function to evaluate.
+    state : BooleanSpace
+        The state in which the function is evaluated.
+
+    Returns
+    -------
+    Literal[0, 1] | None
+        The function value in the given state, or `None` if the function is
+        undetermined.
     """
     if f.is_false():
         return 0
@@ -55,7 +113,18 @@ def function_eval(f: Bdd, state: BooleanSpace) -> Literal[0, 1] | None:
 
 def function_is_true(f: Bdd, state: BooleanSpace) -> bool:
     """
-    Returns `True` if the given BDD function evaluates to `1` for the given
-    state (or space).
+    `True` if the BDD function evaluates to `1` for the given state (or space).
+
+    Parameters
+    ----------
+    f : Bdd
+        The function to evaluate.
+    state : BooleanSpace
+        The state in which the function is evaluated.
+
+    Returns
+    -------
+    bool
+        `True` if the function evaluates to `1` in the given state.
     """
     return function_eval(f, state) == 1
