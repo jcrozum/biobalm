b47f6485d590e611fcb171845adb49f332d754a3
Jordan Rozum <jcrozum@gmail.com>
2024-04-10 15:09:59 -0400

    fix init files for linting

--- biobalm/__init__.py
+++ biobalm/__init__.py
@@ -1,6 +1,8 @@
 from biobalm import control as control  # noqa: F401
 from biobalm import drivers as drivers  # noqa: F401
 from biobalm import types as types  # noqa: F401
-from biobalm.succession_diagram import SuccessionDiagram as SuccessionDiagram  # noqa: F401
+from biobalm.succession_diagram import (  # noqa: F401
+    SuccessionDiagram as SuccessionDiagram,
+)
 
 __version__ = "0.1.0"
